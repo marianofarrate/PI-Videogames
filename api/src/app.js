f91a65f28237063f6c2aa5309a562853e9764fd2
marianofarrate <igorandrev@hotmail.com>
2022-04-04 14:43:18 -0300

    arreglo app.js cors

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,12 @@ const cors = require("cors")
 
 server.name = 'API';
 
-server.use(cors())
+server.use(
+  cors({
+    origin: 'https://pi-videogames-red.vercel.app',
+    credentials: true
+  })
+)
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
